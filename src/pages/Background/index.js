c39e9754c8baab01242feb41db421b1d706fa0c1
Terrence <terrencejamesdiaz@gmail.com>
2021-04-06 12:46:17 -0700

    Add preliminary context menu items, open in new tab

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,26 +1,47 @@
 import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
 
-console.log('Yooo');
-chrome.tabs.onCreated.addListener(function(tab) {
-  console.log("hello");
+
+chrome.contextMenus.create({
+    "title": "Open link in tab group",
+    "onclick": function(e) {
+      openNewTab(e.linkUrl);
+    },
+    "contexts": ["link"]
+})
+
+chrome.contextMenus.create({
+    "title": "Open image in tab group",
+    "onclick": function(e) {
+      console.log(e);
+    },
+    "contexts": ["image"]
+})
+
+function openNewTab(url){
+  // need to pass in groupId somehow, and maybe index?
+  var createProperties = {
+    "active" : false,
+    "url" : url
+  };
+  chrome.tabs.create(createProperties, addToTabGroup);
+}
+
+function addToTabGroup(tab){
   console.log(tab);
+}
+
+chrome.tabs.onCreated.addListener(function(tab) {
   var openerTabId = tab.openerTabId;
   if (openerTabId != null && tab.active == false){
-    console.log("Entering if statement");
     chrome.tabs.get(openerTabId, function(openerTab)
     {
       var groupId = openerTab.groupId;
-      console.log(openerTab);
       var options = {};
       if (groupId != -1){
         console.log("Using previous group")
         options.groupId = groupId;
         options.tabIds = tab.id;
-        console.log(groupId);
-        console.log(tab.id);
-        console.log("options:");
-        console.log(options);
       }
       else{
         console.log("Creating new group");
