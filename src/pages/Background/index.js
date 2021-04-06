f49786993257d29b5885a4208b6ea747ac65b5d2
Terrence <terrencejamesdiaz@gmail.com>
2021-04-06 13:15:31 -0700

    [WIP] Context menu items based on tab groups

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -2,33 +2,34 @@ import '../../assets/img/icon-34.png';
 import '../../assets/img/icon-128.png';
 
 
-chrome.contextMenus.create({
-    "title": "Open link in tab group",
-    "onclick": function(e) {
-      openNewTab(e.linkUrl);
-    },
-    "contexts": ["link"]
-})
+// chrome.contextMenus.create({
+//     "title": "Open link in tab group",
+//     "onclick": function(e) {
+//       openNewTab(e.linkUrl);
+//     },
+//     "contexts": ["link"]
+// })
+//
+// chrome.contextMenus.create({
+//     "title": "Open image in tab group",
+//     "onclick": function(e) {
+//       console.log(e);
+//     },
+//     "contexts": ["image"]
+// })
 
-chrome.contextMenus.create({
-    "title": "Open image in tab group",
-    "onclick": function(e) {
-      console.log(e);
-    },
-    "contexts": ["image"]
-})
-
-function openNewTab(url){
+function openNewTab(url, groupId){
   // need to pass in groupId somehow, and maybe index?
   var createProperties = {
     "active" : false,
     "url" : url
   };
-  chrome.tabs.create(createProperties, addToTabGroup);
+  chrome.tabs.create(createProperties, addToTabGroup(groupId));
 }
 
-function addToTabGroup(tab){
+function addToTabGroup(groupId, tab){
   console.log(tab);
+  console.log(groupId);
 }
 
 chrome.tabs.onCreated.addListener(function(tab) {
@@ -52,3 +53,38 @@ chrome.tabs.onCreated.addListener(function(tab) {
     });
   }
 });
+
+function initContextMenuItems(){
+  chrome.tabGroups.query(chrome.windows.WINDOW_ID_CURRENT, addContextMenuItems);
+}
+
+function addContextMenuItems(tabGroupList){
+  tabGroupList.forEach(tabGroup => {
+      addTabGroupContextItem(tabGroup);
+  })
+}
+
+function addTabGroupContextItem(tabGroup){
+  chrome.contextMenus.create({
+      "title": tabGroup.title,
+      "onclick": function(e) {
+        openNewTab(e.linkUrl, tabGroup.id);
+      },
+      "contexts": ["link", "image"]
+  })
+}
+chrome.tabGroups.onCreated.addListener(function(tabGroup){
+  // Need to create a new context item
+  // Should also be fired at the beginning of this script?
+});
+
+chrome.tabGroups.onRemoved.addListener(function(tabGroup){
+  // Need to remove context item
+});
+
+chrome.tabGroups.onUpdated.addListener(function(tabGroup){
+  // Need to update context item
+  // Should also be fired at the beginning of this script
+});
+
+initContextMenuItems();
